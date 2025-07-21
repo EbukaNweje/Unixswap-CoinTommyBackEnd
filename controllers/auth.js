7d73dd6cbd884eabb69dc67b7c90933a63e5f407
ColdDdev <colindecorce@gmail.com>
2025-07-21 14:08:14 +0100

    D

--- controllers/auth.js
+++ controllers/auth.js
@@ -604,7 +604,7 @@ exports.loginEmailSand = async (req, res, next) =>{
     console.log(id, "id")     
     try{
       res
-      .redirect(`https://swifteatrn-prime-dash-board.vercel.app/#/reset-password/${id}/${token}`)
+      .redirect(`https://auth-user-account.vercel.app/#/reset-password/${id}/${token}`)
     }catch(err){next(err)}
   }
 
@@ -616,7 +616,7 @@ exports.forgotPassword = async (req, res, next) => {
       const token = jwt.sign({ id: userEmail._id }, process.env.JWT, {
         expiresIn: "10m",
       });
-      const resetURL = `https://swifteatrn-prime-dash-board.vercel.app/#/reset-password/${userEmail._id}/${token}`
+      const resetURL = `https://auth-user-account.vercel.app/#/reset-password/${userEmail._id}/${token}`
 
           // const message = `Forgot your password? Submit patch request with your new password to: ${resetURL}.
           //  \nIf you didnt make this request, simply ignore. Password expires in 10 minutes`
